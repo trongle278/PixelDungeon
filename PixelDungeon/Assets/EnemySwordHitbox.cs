5ebe26b9c5cb11dabfd8158051b515f8a118ac0d
Xion Sir <trongle278@gmail.com>
2024-06-28 22:37:00 +0700

    gg

--- PixelDungeon/Assets/EnemySwordHitbox.cs
+++ PixelDungeon/Assets/EnemySwordHitbox.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class EnemySwordHitbox : MonoBehaviour
 {
+    public int enemyattackdamage = 10;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log("Enemy attack: " + enemyattackdamage);
             PlayerStatus playerStatus = other.GetComponent<PlayerStatus>();
             if (playerStatus != null)
             {
-                playerStatus.TakeDamage(10); // Example damage value
+                playerStatus.TakeDamage(enemyattackdamage); // Example damage value
             }
         }
     }
